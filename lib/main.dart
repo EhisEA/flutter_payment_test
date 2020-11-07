19bbcb4a57784c55e0706421471392ab9e6e770b
EhisEA <emmanueleayemere@gmail.com>
2020-11-07 23:44:37 +0100

    first test

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutterwave/flutterwave.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutterwave Beta'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +25,245 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final formKey = GlobalKey<FormState>();
+  final amountController = TextEditingController();
+  final currencyController = TextEditingController();
+  final narrationController = TextEditingController();
+  final publicKeyController = TextEditingController();
+  final encryptionKeyController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String selectedCurrency = "";
+
+  bool isDebug = true;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    this.currencyController.text = this.selectedCurrency;
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        width: double.infinity,
+        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Form(
+          key: this.formKey,
+          child: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.amountController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(hintText: "Amount"),
+                  validator: (value) =>
+                      value.isNotEmpty ? null : "Amount is required",
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.currencyController,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.black),
+                  readOnly: true,
+                  onTap: this._openBottomSheet,
+                  decoration: InputDecoration(
+                    hintText: "Currency",
+                  ),
+                  validator: (value) =>
+                      value.isNotEmpty ? null : "Currency is required",
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.publicKeyController,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Public Key",
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.encryptionKeyController,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Encryption Key",
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.emailController,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  ),
+                  validator: (value) =>
+                      value.isNotEmpty ? null : "Email is required",
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: TextFormField(
+                  controller: this.phoneNumberController,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Phone Number",
+                  ),
+                  validator: (value) =>
+                      value.isNotEmpty ? null : "Phone Number is required",
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Use Debug"),
+                    Switch(
+                      onChanged: (value) => {
+                        setState(() {
+                          isDebug = value;
+                        })
+                      },
+                      value: this.isDebug,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: RaisedButton(
+                  onPressed: this._onPressed,
+                  color: Colors.blue,
+                  child: Text(
+                    "Make Payment",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _onPressed() {
+    if (this.formKey.currentState.validate()) {
+      this._handlePaymentInitialization();
+    }
+  }
+
+  _handlePaymentInitialization() async {
+    final flutterwave = Flutterwave.forUIPayment(
+        amount: this.amountController.text.toString().trim(),
+        currency: this.currencyController.text,
+        context: this.context,
+        publicKey: this.publicKeyController.text.trim(),
+        encryptionKey: this.encryptionKeyController.text.trim(),
+        email: this.emailController.text.trim(),
+        fullName: "Test User",
+        txRef: DateTime.now().toIso8601String(),
+        narration: "Example Project",
+        isDebugMode: this.isDebug,
+        phoneNumber: this.phoneNumberController.text.trim(),
+        acceptAccountPayment: true,
+        acceptCardPayment: true,
+        acceptUSSDPayment: true);
+    final response = await flutterwave.initializeForUiPayments();
+    if (response != null) {
+      this.showLoading(response.data.status);
+    } else {
+      this.showLoading("No Response!");
+    }
+  }
+
+  void _openBottomSheet() {
+    showModalBottomSheet(
+        context: this.context,
+        builder: (context) {
+          return this._getCurrency();
+        });
+  }
+
+  Widget _getCurrency() {
+    final currencies = [
+      FlutterwaveCurrency.UGX,
+      FlutterwaveCurrency.GHS,
+      FlutterwaveCurrency.NGN,
+      FlutterwaveCurrency.RWF,
+      FlutterwaveCurrency.KES,
+      FlutterwaveCurrency.XAF,
+      FlutterwaveCurrency.XOF,
+      FlutterwaveCurrency.ZMW
+    ];
+    return Container(
+      height: 250,
+      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+      color: Colors.white,
+      child: ListView(
+        children: currencies
+            .map((currency) => ListTile(
+                  onTap: () => {this._handleCurrencyTap(currency)},
+                  title: Column(
+                    children: [
+                      Text(
+                        currency,
+                        textAlign: TextAlign.start,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      SizedBox(height: 4),
+                      Divider(height: 1)
+                    ],
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  _handleCurrencyTap(String currency) {
+    this.setState(() {
+      this.selectedCurrency = currency;
+      this.currencyController.text = currency;
+    });
+    Navigator.pop(this.context);
+  }
+
+  Future<void> showLoading(String message) {
+    return showDialog(
+      context: this.context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
+            width: double.infinity,
+            height: 50,
+            child: Text(message),
+          ),
+        );
+      },
     );
   }
 }
